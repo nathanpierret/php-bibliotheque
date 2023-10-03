97e22697674fab5482b374e39075063160946d66
Nathan Pierret <pierretnathan49@gmail.com>
2023-10-03 10:31:17 +0200

    Ajout Adhérent et travail sur Emprunt

---
+++ tests/unitaires/emprunt-test.php
@@ -0,0 +1,12 @@
+<?php
+
+require "./vendor/autoload.php";
+require "./tests/utils/couleurs.php";
+
+echo PHP_EOL;
+echo GREEN_BACKGROUND.BLACK;
+echo "Tests : classe Emprunt";
+echo RESET;
+echo PHP_EOL;
+
+echo "Test : ";
